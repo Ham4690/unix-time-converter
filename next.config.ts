3d944343f84127f0c5ce25bfc81efe513ce572b8
Arata Higashiguchi <ham46903@gmail.com>
2025-03-15 14:53:23 +0900

    Add configuration for Github Pages deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: "/unix-time-converter",
+  trailingSlash: true,
 };
 
 export default nextConfig;
